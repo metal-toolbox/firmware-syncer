f3289d05a0e2e8f8d6acb549f9a5cb6f3458e2bb
Diogo Mendes Matsubara <diogo.mendesmatsubara@eu.equinix.com>
2022-06-24 09:50:37 +0200

    Remove vendor/ from repository

--- vendor/github.com/rclone/rclone/backend/local/encode_other.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows && !darwin
-// +build !windows,!darwin
-
-package local
-
-import "github.com/rclone/rclone/lib/encoder"
-
-// This is the encoding used by the local backend for non windows platforms
-const defaultEnc = encoder.Base

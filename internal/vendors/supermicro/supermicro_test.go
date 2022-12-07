32ceb9210ae8a807eef783860fec512189fe7cfa
Diogo Mendes Matsubara <diogo.mendesmatsubara@eu.equinix.com>
2022-12-07 13:30:24 +0100

    Add code to unzip firmware archive
    Not entirely working yet but test cases will help get there

--- internal/vendors/supermicro/supermicro_test.go
+++ internal/vendors/supermicro/supermicro_test.go
@@ -1,7 +1,10 @@
 package supermicro
 
 import (
+	"fmt"
 	"io"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -61,4 +64,85 @@ MD5 CheckSum: 3f5cecadf92192d86d049a99b36939ab
 			assert.Equal(t, tc.wantFilename, filename)
 		})
 	}
+}
+
+func Test_unzipFirmwareBinary(t *testing.T) {
+	cases := []struct {
+		name             string
+		zipArchivePath   string
+		firmwareFilename string
+		firmwareChecksum string
+	}{
+		{
+			// foobar.zip
+			//  |-foobar.bin
+			"archive name matches firmware name",
+			getPathToFixture("foobar.zip"),
+			"foobar.bin",
+			"14758f1afd44c09b7992073ccf00b43d",
+		},
+		{
+			// foobar2.zip
+			//  |-foobar/foobar.bin
+			"firmware file inside dir in archive",
+			getPathToFixture("foobar2.zip"),
+			"foobar.bin",
+			"14758f1afd44c09b7992073ccf00b43d",
+		},
+		{
+			// foobar3.zip
+			//  |-foobar/foobar.zip
+			"nested zip firmware file inside dir in archive",
+			getPathToFixture("foobar3.zip"),
+			"foobar.bin",
+			"14758f1afd44c09b7992073ccf00b43d",
+			// Archive:  X11DPT-B(H)_3_4_AS17305_SUM250.zip
+			//  Length      Date    Time    Name
+			//  ---------  ---------- -----   ----
+			//		  0  11-23-2020 10:18   X11DPT-B(H)_3.4_AS17305_SUM250/
+			//   29625347  09-21-2020 11:52   X11DPT-B(H)_3.4_AS17305_SUM250/SMT_X11AST2500_173_05.zip
+			//   19892213  09-08-2020 11:00   X11DPT-B(H)_3.4_AS17305_SUM250/sum_2.5.0_BSD_x86_64_20200722.tar.gz
+			//   11280455  09-08-2020 11:00   X11DPT-B(H)_3.4_AS17305_SUM250/sum_2.5.0_Linux_x86_64_20200722.tar.gz
+			//	8952902  09-08-2020 11:00   X11DPT-B(H)_3.4_AS17305_SUM250/sum_2.5.0_Win_x86_64_20200722.zip
+			//	   2422  11-23-2020 10:19   X11DPT-B(H)_3.4_AS17305_SUM250/X11DPT-B(H)_Software_Package_Readme.txt
+			//   10455672  11-03-2020 09:34   X11DPT-B(H)_3.4_AS17305_SUM250/X11DPTB0.B03.zip
+			//  ---------                     -------
+			//   80209011                     7 files
+
+		},
+		{
+			// foobar4.zip
+			//  |-foo.bar
+			"firmware without bin extension",
+			getPathToFixture("foobar4.zip"),
+			"foo.bar",
+			"14758f1afd44c09b7992073ccf00b43d",
+			// Archive:  H12SSW0_528.zip
+			//  Length      Date    Time    Name
+			//  ---------  ---------- -----   ----
+			//	 570480  05-03-2019 15:21   afuefi.smc
+			//	   1149  02-07-2018 19:56   flash.nsh
+			//   33554432  05-28-2020 14:35   H12SSW0.528
+			//	   1629  06-21-2019 14:24   Readme for H12 AMI BIOS-UEFI.txt
+			//  ---------                     -------
+			//   34127690                     4 files
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			f, err := unzipFirmwareBinary(tc.zipArchivePath, tc.firmwareFilename, tc.firmwareChecksum)
+			if err != nil {
+				assert.EqualError(t, err, "some error")
+				return
+			}
+			assert.Equal(t, tc.firmwareFilename, filepath.Base(f.Name()))
+			// Remove the unzipped file from the filesystem
+			os.Remove(f.Name())
+		})
+	}
+}
+
+func getPathToFixture(fixture string) string {
+	p, _ := filepath.Abs(fmt.Sprintf("fixtures/%s", fixture))
+	return p
 }

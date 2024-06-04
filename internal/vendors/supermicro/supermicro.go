7aaa3cdbe6cc5911bb49b66d9469081620c8353e
Olivier FAURAX <olivier.faurax@eu.equinix.com>
2024-06-04 14:53:54 +0000

    supermicro: servervice -> fleetdbapi

--- internal/vendors/supermicro/supermicro.go
+++ internal/vendors/supermicro/supermicro.go
@@ -30,7 +30,7 @@ func NewSupermicroDownloader(logger *logrus.Logger) vendors.Downloader {
 
 // Download will download a file for the given firmware to the given downloadDir,
 // and will return the full path to the downloaded file.
-func (d *Downloader) Download(ctx context.Context, downloadDir string, firmware *serverservice.ComponentFirmwareVersion) (string, error) {
+func (d *Downloader) Download(ctx context.Context, downloadDir string, firmware *fleetdbapi.ComponentFirmwareVersion) (string, error) {
 	urlSplit := strings.Split(firmware.UpstreamURL, "=")
 
 	if len(urlSplit) < 2 {
